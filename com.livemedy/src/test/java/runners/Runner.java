4b760284ca6e102e154152af97b0b163e81ae9ff
unknown <fkarataslioglu@gmail.com>
2024-11-18 23:26:59 +0300

    runner json eklendi

--- com.livemedy/src/test/java/runners/Runner.java
+++ com.livemedy/src/test/java/runners/Runner.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin ={"html:target/cucumber-reports.html",
+                "pretty",
+                "json:target/cucumber-reports/cucumber.json",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
-                "pretty"},
+                },
         monochrome = true,
         features = "src/test/resources/features",
         glue = "stepdefinitions",
